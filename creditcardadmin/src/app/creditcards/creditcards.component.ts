fb15dcdef67844fbaa67ee74335d6d3ac765b818
GaneshPate <grp31886@gmail.com>
2024-07-23 15:57:47 +0530

    CreditCards component added

---
+++ creditcardadmin/src/app/creditcards/creditcards.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core'; 
+
+@Component({
+  selector: 'app-creditcards',
+  standalone: true,
+  imports: [],
+  templateUrl: './creditcards.component.html',
+  styleUrl: './creditcards.component.css'
+})
+export class CreditcardsComponent {
+
+}
